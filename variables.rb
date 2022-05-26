03a7b34848755a596a5c1dcc2cc038862752babb
Myntsu <dingodille16@hotmail.com>
2022-05-26 12:42:10 -0400

    Adding variable sample

---
+++ variables.rb
@@ -0,0 +1,20 @@
+x = 10
+y = 20
+suma = x + y
+puts suma
+
+precio_usd = 20
+precio_mx = 10
+precio_clp = 30
+
+conversion = precio_usd * precio_mx - precio_clp
+puts conversion
+
+texto_saludo = "Hola desde Peru"
+puts texto_saludo
+
+puts "Hasta el proximo codigo"
+
+puts "Hasta el"
+puts            "proximo"
+puts                        "codigo 2"
