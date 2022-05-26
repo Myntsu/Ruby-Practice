316c126c9c7f423490241401709909a106ec3424
Myntsu <dingodille16@hotmail.com>
2022-05-26 13:17:43 -0400

    Adding numbers sample

---
+++ numbers.rb
@@ -0,0 +1,26 @@
+numero1 = 20
+numero2 = 30
+numero3 = 10.1
+numero4 = 39.3
+
+suma_interger = numero1 + numero2
+suma_float = numero3 + numero4
+suma_ambos = suma_interger + suma_float
+
+puts suma_interger
+puts suma_float
+puts suma_ambos
+
+puts numero1.class
+puts numero2.class
+puts numero3.class
+puts numero4.class
+
+puts suma_interger.class
+puts suma_float.class
+puts suma_ambos.class
+
+# De interger a float
+numero5 = 3/4.to_f
+puts numero5
+puts numero5.class
