cfba9942283227ae70761f6cd4ad5320c9c80fbb
Myntsu <dingodille16@hotmail.com>
2022-05-27 14:14:36 -0400

    Calculator

--- gets.rb
+++ gets.rb
@@ -1,4 +1,12 @@
-print "Cual es tu nombre:"
-respuesta = gets
+# Name
+print "Cual es tu nombre: "
+respuesta = gets.chomp
 puts "Hola #{respuesta}, ¿ya estás listo para las vacaciones?"
-puts "Hola Jose, ¿ya estás listo para las vacaciones?"
+
+# Number
+print "Dame un numero: "
+numero1 = gets.chomp.to_i
+print "Dame otro numero: "
+numero2 = gets.chomp.to_i
+
+puts suma = numero1 + numero2
