c800e4cbc33eaf3465b29214ec185f779b90b2e3
Myntsu <dingodille16@hotmail.com>
2022-06-06 20:04:22 -0400

    Desafio Evaluado - El mayor de 4 números.

---
+++ Desafío evaluado - El mayor de cuatro números/mayor_de_4.rb
@@ -0,0 +1,26 @@
+numero_1 = ARGV[0].to_f
+numero_2 = ARGV[1].to_f
+numero_3 = ARGV[2].to_f
+numero_4 = ARGV[3].to_f
+
+if numero_1 > numero_2 && numero_1 > numero_3 && numero_1 > numero_4
+    puts "--------------------------------------"
+    puts "El número mayor es #{(numero_1).round(0)}."
+    puts "--------------------------------------"
+elsif numero_2 > numero_1 && numero_2 > numero_3 && numero_2 > numero_4
+    puts "--------------------------------------"
+    puts "El número mayor es #{(numero_2).round(0)}."
+    puts "--------------------------------------"
+elsif numero_3 > numero_1 && numero_3 > numero_2 && numero_3 > numero_4
+    puts "--------------------------------------"
+    puts "El número mayor es #{(numero_3).round(0)}."
+    puts "--------------------------------------"
+elsif numero_4 > numero_1 && numero_4 > numero_2 && numero_4 > numero_3
+    puts "--------------------------------------"
+    puts "El número mayor es #{(numero_4).round(0)}."
+    puts "--------------------------------------"
+else
+    puts "--------------------------------------"
+    puts "Los números tienen que ser distintos."
+    puts "--------------------------------------"
+end
