d7ef76c51f653675aaa32b1e2b0746c672231d0e
Myntsu <dingodille16@hotmail.com>
2022-06-07 12:27:13 -0400

    Example iterators in Ruby.

---
+++ iterator.rb
@@ -0,0 +1,5 @@
+5.times{puts "Hola desde Chile."}
+1.upto(5){puts "Hola desde Perú."}
+5.downto(1){puts "Hola desde Colombia."}
+(1..5).each{puts "Hola desde Argentina."}
+(1..5).each{print "Hola desde Argentina."}
