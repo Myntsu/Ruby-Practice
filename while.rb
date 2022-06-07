3d18a28072babc0dda7ebddcb85322f421a60f57
Myntsu <dingodille16@hotmail.com>
2022-06-07 12:00:02 -0400

    While example in Ruby.

---
+++ while.rb
@@ -0,0 +1,14 @@
+i = 5
+while i > 0
+    puts "Conteo: #{i}"
+    i -= 1
+end
+    puts "¡Despegando!"
+
+j = 0
+add = 0
+while j < 10
+    add += j
+    j += 1
+end
+    puts "La suma de los primero 10 números es #{add}."
