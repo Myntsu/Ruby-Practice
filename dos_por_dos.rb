d63696d9a53a3c5363e51e2d7e3f3001df62bb75
Myntsu <dingodille16@hotmail.com>
2022-06-09 00:53:59 -0400

    Details

---
+++ dos_por_dos.rb
@@ -0,0 +1,7 @@
+print "Ingresa los numeros: "
+n = gets.chomp.to_i
+
+(1..n).each do |i|
+    print "*"
+    print "."
+end
---
+++ puntos_intercalados.rb
@@ -0,0 +1,9 @@
+print "Ingresa los numeros: "
+n = gets.chomp.to_i
+(1..n).each do |var_block|
+    if var_block.even?
+        print var_block
+    else
+        print "."
+    end
+end
