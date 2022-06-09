d825fccd6255ffa78dcd3534e632d0e5942a1d05
Myntsu <dingodille16@hotmail.com>
2022-06-09 10:14:30 -0400

    Simple array

---
+++ simple_array.rb
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 100, 500, "hola", "chao"]
+
+print a
+puts ""
+puts a[0]
+puts a[5]
+puts a[-2]
+print a[2,5]
+print a[-3,4]
+puts ""
+puts "#{a[0]} #{a[3]}"
+
+puts ""
+
+variable_1 = a
+b = ["caliente", "frio", 3, variable_1, 70, "come rico"]
+
+print b
